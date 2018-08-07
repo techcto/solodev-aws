af2cf993a0eb3a9fb5b04eedf3b88749228c2f4c
ocoa <shawn@ocoa.com>
2018-08-07 13:43:06 -0400

    fix broken dir

--- scripts/install-node.sh
+++ scripts/install-node.sh
@@ -1,5 +1,6 @@
 #Install Node
 mkdir -p /var/www/.npm
+mkdir -p /var/www/.npmrc
 echo 'export NODE_PATH=/var/www/.npm-global/lib/node_modules' >> /var/www/.npmrc
 echo 'export PATH=$PATH:/var/www/.npm-global/bin' >> /var/www/.npmrc
 export PATH=/var/www/.npm-global/bin:$PATH
