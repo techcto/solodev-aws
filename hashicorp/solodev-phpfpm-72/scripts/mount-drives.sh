17f968a6653ae9e2345c7a866cf0a0df0a0bf306
ocoa <shawn@ocoa.com>
2018-08-08 15:47:21 -0400

    ...

--- hashicorp/solodev-phpfpm-72/scripts/mount-drives.sh
+++ hashicorp/solodev-phpfpm-72/scripts/mount-drives.sh
@@ -1,9 +1,14 @@
-# mkfs.ext4 /dev/xvdb
-# echo '/dev/xvdb /var/www/Solodev/clients/solodev ext4 defaults,auto,noexec 0 0'
-# mount -a
-# blockdev --setra 32 /dev/xvdb
-# /bin/dd if=/dev/zero of=/mnt/swapfile bs=1M count=2048
-# chown root:root /mnt/swapfile
-# chmod 600 /mnt/swapfile
-# /sbin/mkswap /mnt/swapfile
-# /sbin/swapon /mnt/swapfile
+#Client Drive
+mkfs.ext4 /dev/xvdb
+echo '/dev/xvdb /var/www/Solodev/clients/solodev ext4 defaults,auto,noexec 0 0'
+blockdev --setra 32 /dev/xvdb
+
+#Swap
+/bin/dd if=/dev/zero of=/mnt/swapfile bs=1M count=2048
+chown root:root /mnt/swapfile
+chmod 600 /mnt/swapfile
+/sbin/mkswap /mnt/swapfile
+/sbin/swapon /mnt/swapfile
+
+#Mount
+mount -a
