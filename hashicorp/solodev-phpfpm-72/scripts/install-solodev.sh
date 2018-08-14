49e621e5c391e44beac3c7a2d18a786fc85ef797
ocoa <shawn@ocoa.com>
2018-08-13 21:28:05 -0400

    ...

--- hashicorp/solodev-phpfpm-72/scripts/install-solodev.sh
+++ hashicorp/solodev-phpfpm-72/scripts/install-solodev.sh
@@ -1,7 +1,7 @@
 #Install Solodev
 mkdir -p /tmp/Solodev
-fn="$(aws s3 ls s3://solodev-bamboo | sort | tail -n 1 | awk '{print $4}')"
-aws s3 cp s3://solodev-bamboo/$fn /tmp/Solodev/Solodev.zip
+fn="$(aws s3 ls s3://solodev-release | sort | tail -n 1 | awk '{print $4}')"
+aws s3 cp s3://solodev-release/$fn /tmp/Solodev/Solodev.zip
 cd /tmp/Solodev/
 unzip Solodev.zip
 rm -Rf Solodev.zip
