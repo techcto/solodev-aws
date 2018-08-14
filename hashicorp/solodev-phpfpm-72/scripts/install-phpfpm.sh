7fe02974680822aa8181cb95b08eb557160d60eb
ocoa <shawn@ocoa.com>
2018-08-14 18:30:43 -0400

    fix php sock

--- hashicorp/solodev-phpfpm-72/scripts/install-phpfpm.sh
+++ hashicorp/solodev-phpfpm-72/scripts/install-phpfpm.sh
@@ -29,9 +29,10 @@ echo "AddType text/html .stml .php" >> /etc/httpd/conf.d/php72-php.conf
 echo "DirectoryIndex index.stml index.php" >> /etc/httpd/conf.d/php72-php.conf
 echo 'SetEnvIfNoCase ^Authorization$ "(.+)" HTTP_AUTHORIZATION=$1' >> /etc/httpd/conf.d/php72-php.conf
 echo "<FilesMatch \.(php|phar|stml)$>" >> /etc/httpd/conf.d/php72-php.conf
-echo ' SetHandler "proxy:unix:/run/php-fpm/www.sock|fcgi://localhost"' >> /etc/httpd/conf.d/php72-php.conf
+echo ' SetHandler "proxy:unix:/run/www.sock|fcgi://localhost"' >> /etc/httpd/conf.d/php72-php.conf
 echo "</FilesMatch>" >> /etc/httpd/conf.d/php72-php.conf
 echo "security.limit_extensions = .php .stml" >> /etc/opt/remi/php72/php-fpm.d/www.conf
+echo "listen = /run/www.sock" >> /etc/opt/remi/php72/php-fpm.d/www.conf
 echo "listen.owner = apache" >> /etc/opt/remi/php72/php-fpm.d/www.conf
 echo "listen.mode = 0660" >> /etc/opt/remi/php72/php-fpm.d/www.conf
 echo "chdir = /var/www/Solodev" >> /etc/opt/remi/php72/php-fpm.d/www.conf
