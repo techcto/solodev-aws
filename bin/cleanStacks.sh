060eb771eec8420ce5bf3d582aaf56c213ed806d
ocoa <shawn@ocoa.com>
2018-10-09 17:00:47 -0400

    ..

--- bin/cleanStacks.sh
+++ bin/cleanStacks.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo $(aws cloudformation describe-stacks)
-for STACK in $(aws cloudformation describe-stacks | ./jq -r '.StackSummaries[].StackName')
+for STACK in $(aws cloudformation describe-stacks | ./jq -r '.Stacks[].StackName')
 do
     echo $STACK
 done
