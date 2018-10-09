22a956fa9b10427de11fc38cae0d1ee1391a0a5a
ocoa <shawn@ocoa.com>
2018-10-09 17:17:47 -0400

    ..

--- bin/cleanStacks.sh
+++ bin/cleanStacks.sh
@@ -4,7 +4,7 @@
 for STACK in $(aws cloudformation describe-stacks | ./jq -r '.Stacks[].StackName')
 do
     if [[ $STACK == *"-tmp-"* ]]; then
-        echo "I found one. Time to delete stack.  Bye-Bye!"
+        echo "I found one. Time to delete stack: ${STACK}.  Bye-Bye!"
         aws cloudformation delete-stack --stack-name $STACK
     fi
 done
