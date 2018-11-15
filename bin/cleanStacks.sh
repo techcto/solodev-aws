f25b4841025f73f2b50da50baf4570ec06a1b36c
ocoa <shawn@ocoa.com>
2018-11-15 13:37:38 -0500

    ..

--- bin/cleanStacks.sh
+++ bin/cleanStacks.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #echo $(aws cloudformation describe-stacks)
-for STACK in $(aws cloudformation describe-stacks | ./jq -r '.Stacks[].StackName')
+for STACK in $(aws cloudformation describe-stacks --region us-east-1 us-west-1 | ./jq -r '.Stacks[].StackName')
 do
     if [[ $STACK == *"-tmp-"* ]]; then
         echo "I found one. Time to delete stack: ${STACK}.  Bye-Bye!"
