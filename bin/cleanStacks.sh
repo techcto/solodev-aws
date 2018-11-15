d828a76926e6d588823ed1279fe5956b49ee04a0
ocoa <shawn@ocoa.com>
2018-11-15 13:28:56 -0500

    ..

--- bin/cleanStacks.sh
+++ bin/cleanStacks.sh
@@ -5,6 +5,6 @@ for STACK in $(aws cloudformation describe-stacks | ./jq -r '.Stacks[].StackName
 do
     if [[ $STACK == *"-tmp-"* ]]; then
         echo "I found one. Time to delete stack: ${STACK}.  Bye-Bye!"
-        aws cloudformation delete-stack --regions "us-east-1" "us-west-1" --stack-name $STACK
+        aws cloudformation delete-stack --region "us-east-1" "us-west-1" --stack-name $STACK
     fi
 done
